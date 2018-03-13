aa22bffaa0c59dece27f081848613ac736c3ac4a
NingSun <ning.sun@intel.com>
2018-03-12 18:25:03 -0700

    Remove win32 support in SoftHSMv2
    Due to license issue, we have to remove win32 support in SoftHSMv2.
    
    Issue-ID: AAF-151
    Change-Id: I7894a3482ab221f3fdc2fcc4004d5e104678952b
    Signed-off-by: NingSun <ning.sun@intel.com>

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/etc/bash
+
+sudo systemctl stop apt-daily.timer
+
+sudo ps -A | grep apt > apt-process
+cat apt-process
+ps -ef | grep your_process_name | grep -v grep | awk '{print $2}' | xargs kill -9
+sudo ps -A | grep apt > apt-process
+cat apt-process
+ps -ef | grep your_process_name | grep -v grep | awk '{print $2}' | xargs kill -9
+#sudo systemctl start apt-daily.timer
+sudo rm /var/lib/dpkg/lock
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+echo "remove locks ..."
+echo "==========================================================="
+sudo apt -y update
+sudo apt-get dist-upgrade
+sudo dpkg --configure -a
+
+#sudo apt -y upgrade
+#sudo dpkg --configure -a
+#sudo apt -y autoremove
+#echo "upgrade done ..."
+echo "install autotools ..."
+echo "==========================================================="
+sudo apt-get -y install autoconf
+sudo apt-get -y install automake
+sudo apt-get -y install libtool
+sudo apt-get -y install autotools-dev
+sudo apt -y autoremove
+echo "install tpm2-tools ..."
+echo "==========================================================="
+sudo apt -y install tpm2-tools
+sudo find / |grep tpm20.h
+echo "================== list /usr/local ================"
+echo "==========================================================="
+sudo ls -l /usr/local
+echo "==================== list /usr/include ================"
+echo "==========================================================="
+sudo ls -l /usr/include
+echo "=============== list /usr/local/include ================"
+echo "==========================================================="
+
+sudo ls -l /usr/local/include
+
+echo "Build SoftHSMv2..."
+echo "==========================================================="
+cd SoftHSMv2
+sh autogen.sh
+./configure
+make
+cd ..
+
+echo "Build TPM2_plugin..."
+echo "==========================================================="
+cd TPM2-Plugin
+./bootstrap
+./configure
+make
+cd ..
+
+
