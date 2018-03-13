c687e987514e51951f2d2774e02cc5838d4e4948
NingSun <ning.sun@intel.com>
2018-03-12 18:25:03 -0700

    Remove win32 support in SoftHSMv2
    Due to license issue, we have to remove win32 support in SoftHSMv2.
    
    Issue-ID: AAF-151
    Change-Id: I7894a3482ab221f3fdc2fcc4004d5e104678952b
    Signed-off-by: NingSun <ning.sun@intel.com>

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/etc/bash
+
+#set -e
+
+#sudo rm /var/lib/dpkg/lock
+#sudo rm /var/cache/apt/archives/lock
+sudo dpkg --configure -a
+sudo apt-get -y install autoconf
+sudo apt-get -y install automake
+sudo apt-get -y install libtool
+sudo apt-get -y install autotools-dev
+
+
+echo "Build SoftHSMv2..."
+cd SoftHSMv2
+sh autogen.sh
+./configure
+make
+cd ..
+
+echo "Build TPM2_plugin..."
+cd TPM2-Plugin
+./bootstrap
+./configure
+make
+cd ..
+
+
