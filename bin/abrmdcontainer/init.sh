6e52ad0d56ea177c8d3ef80b405058ad6c229838
Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>
2018-08-02 10:41:31 -0700

    Containerize TPM Initialization tool
    Modifying ABRMD container to support Init tool
    
    Change-Id: I8b2f8171688b67567e3ad4a3e4942ae76737bdfc
    Issue-ID: AAF-342
    Signed-off-by: Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>

---
+++ bin/abrmdcontainer/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# Start DBUS
+mkdir -p /var/run/dbus
+stdbuf -oL -eL dbus-daemon --system --nofork 2>&1 1> /var/log/dbus-daemon.log &
+
+# Start Resource Manager
+if [ -z $TPM_SIMULATOR ]; then
+  echo "Using TPM Hardware for the operations";
+  tpm2-abrmd
+else
+  echo "Using TPM Simulator for the opeations";
+  hostip=$(ip route show | awk '/default/ {print $3}');
+  echo "Connecting to $hostip\n";
+  tpm2-abrmd -a $hostip -t socket&
+fi
+
+/abrmd/bin/initialize_tpm.sh
+status=$?
+if [ $status -eq "0" ]; then
+ echo "TPM Initialization successful $status"
+fi
+exit $?
