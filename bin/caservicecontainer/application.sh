c3802a340b237b7cfca8c9267d7358151dffc27e
Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>
2018-08-15 11:45:57 -0700

    Enhancing the test scipt to support SOFTHSM
    Issue-ID: AAF-414
    Change-Id: I237e789511b28652d6c6de3f4110481aa499294f
    Signed-off-by: Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>

---
+++ bin/caservicecontainer/application.sh
@@ -0,0 +1,29 @@
+# Setting up the java application and running the application
+# 1. Create the configuration pkcs11.cfg for the application
+    touch /tmp/pkcs11.cfg
+    chmod 755 /tmp/pkcs11.cfg
+    echo "name = ${key_label}" >> /tmp/pkcs11.cfg
+    echo "${applicationlibrary}"
+    echo "library = ${applicationlibrary}" >> /tmp/pkcs11.cfg
+    echo "slot = ${SoftHSMv2SlotID}" >> /tmp/pkcs11.cfg
+
+# 2. Compile the Application
+cd /tmp/files
+cp test.csr /tmp/test.csr
+javac CaSign.java
+
+# 3. Convert the crt to der format
+openssl x509 -in ca.cert -outform der -out ca.der
+
+# 4. Add the ca certificate
+pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so -l --pin ${upin} --write-object ./ca.der --type cert --id ${cert_id}
+
+# 5. Run the Application
+java CaSign ${upin} 0x${cert_id}
+
+# 6. Verify the generated certificate
+openssl verify -verbose -CAfile ca.cert /tmp/test.cert
+
+# 7. CleanUp
+cd /
+rm -rf slotinfo.txt
--- bin/caservicecontainer/test.sh
+++ bin/caservicecontainer/test.sh
@@ -1,4 +1,3 @@
-
 #NOTE - This scripts expects the Init and the Duplicate tools to be already run and the output files to be present at the
 # shared volume (input for Import tool)
 
@@ -6,104 +5,102 @@
 set -e
 
 #Placeholder of Input files to the Import tool which is the output of duplicate tool
-duplicatetooldir="/tmp/files/duplicatetoolfiles"
+export duplicatetooldir="/tmp/files"
 #Pluginlibrary
-pluginlibrary="/usr/local/lib/libtpm2-plugin.so"
+export pluginlibrary="/usr/local/lib/libtpm2-plugin.so"
 #key_id is the parameter expected by SoftHSM
-key_id="8738"
+export key_id="8738"
 #TPM handle
-tpm_handle="0x81000011"
+export tpm_handle="0x81000011"
 #Key_label is the  parameter expected by SoftHSM
-key_label="ABC"
+export key_label="ABC"
 #UserPin for the SoftHSM operations
-upin="123456789"
+export upin="123456789"
 #SoPin for the SoftHSM operations
-sopin="123456789"
+export sopin="123456789"
 #Slot number for the SoftHSM operations. Initially it should be started with 0
-slot_no="0"
+export slot_no="0"
 #Name for creating the slot used by SoftHSM
-token_no="Token1"
+export token_no="Token1"
 #Location for the application to fecth SoftHSM library
-applicationlibrary="/usr/local/lib/softhsm/libsofthsm2.so"
+export applicationlibrary="/usr/local/lib/softhsm/libsofthsm2.so"
 #cert_id is the input for the application which is hexadecimal equivalent of key_id
-cert_id=$(printf '%x' ${key_id})
+export cert_id=$(printf '%x' ${key_id})
 
-#Plugin directory for the SoftHSM to load plugin and for further operations
-if [ ! "$SSHSM_HW_PLUGINS_PARENT_DIR" ]
-then
-    echo "There is no Plugin directory assigned. Hence creating the directory required by SoftHSM"
-    SSHSM_HW_PLUGINS_PARENT_DIR="/tmp/hwparent"
-fi
+# Plugin directory for the SoftHSM to load plugin and for further operations
+if [ ${SSHSM_HW_PLUGINS_PARENT_DIR} -a -f ${duplicatetooldir}/out_parent_public ]; then
+
+    # 1.a Plugin Implementation
+    echo "Plugin directory available. Hence creating the required directory"
+    export SSHSM_HW_PLUGINS_PARENT_DIR="/tmp/hwparent"
     mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}
-echo "The newly assigned plugin directory is ${SSHSM_HW_PLUGINS_PARENT_DIR}"
+    echo "The newly assigned plugin directory is ${SSHSM_HW_PLUGINS_PARENT_DIR}"
 
-mkdir -p /var/run/dbus
+    mkdir -p /var/run/dbus
 
-# Initialize the token
-softhsm2-util --init-token --slot ${slot_no} --label "${token_name}" --pin ${upin} --so-pin ${sopin}
-softhsm2-util --show-slots | grep 'Slot ' | cut -d\           -f2 | head -1 >> slotinfo.txt
-SoftHSMv2SlotID="$(cat slotinfo.txt)"
-echo "${SoftHSMv2SlotID}"
+    # 1.b  Initialize the token
+    softhsm2-util --init-token --slot ${slot_no} --label "${token_name}" --pin ${upin} --so-pin ${sopin}
+    softhsm2-util --show-slots | grep 'Slot ' | cut -d\           -f2 | head -1 >> slotinfo.txt
+    export SoftHSMv2SlotID="$(cat slotinfo.txt)"
+    echo "${SoftHSMv2SlotID}"
 
-# 1. Create the directory as expected by the SoftHSM to read the files
-mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm
-mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate
-mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01
+    # 1.c Create the directory as expected by the SoftHSM to read the files
+    mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm
+    mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate
+    mkdir -p ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01
 
-cp ${pluginlibrary} ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/plugin.so
-touch ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
-chmod 755 ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
-echo "$tpm_handle" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
+    cp ${pluginlibrary} ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/plugin.so
+    touch ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
+    chmod 755 ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
+    echo "$tpm_handle" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/activate/Afile1.id1
 
-# 2. Generate the pkcs11.cfg file required for the SoftHSM opeations
-touch ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-chmod 755 ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-echo "${key_id}"
-echo "key_id:${key_id}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-echo "${key_label}"
-echo "key_label:${key_label}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-echo "upin:${upin}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-echo "sopin:${sopin}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
-echo "sopin is ${sopin}"
-echo "slot:${SoftHSMv2SlotID}" >>  ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    # 1.d Generate the pkcs11.cfg file required for the SoftHSM operations
+    touch ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    chmod 755 ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    echo "${key_id}"
+    echo "key_id:${key_id}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    echo "${key_label}"
+    echo "key_label:${key_label}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    echo "upin:${upin}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    echo "sopin:${sopin}" >> ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
+    echo "sopin is ${sopin}"
+    echo "slot:${SoftHSMv2SlotID}" >>  ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/pkcs11.cfg
 
-# 3.a Copy the required input files for the Import tool
-cp ${duplicatetooldir}/* /sshsm/tpm-util/initandverify
+    # 1.e Copy the required input files for the Import tool
+    cp ${duplicatetooldir}/* /sshsm/tpm-util/initandverify
 
-# 3.b Run the Import Utility
-cd /sshsm/tpm-util/initandverify
-./ImportTpmKey.sh
+    # 1.f Run the Import Utility
+    cd /sshsm/tpm-util/initandverify
+    ./ImportTpmKey.sh
 
-# 3.c Copy the output of the Import utility to the directory that SoftHSMv2 expects
-cp /sshsm/tpm-util/initandverify/outPriv ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/Kfile1.priv
-cp /sshsm/tpm-util/initandverify/outPub ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/Kfile1.pub
+    # 1.g Copy the output of the Import utility to the directory that SoftHSMv2 expects
+    cp /sshsm/tpm-util/initandverify/outPriv ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/Kfile1.priv
+    cp /sshsm/tpm-util/initandverify/outPub ${SSHSM_HW_PLUGINS_PARENT_DIR}/S01tpm/key01/Kfile1.pub
 
-# Setting up the java application and running the application
-# 4. Create the configuration pkcs11.cfg for the application
-touch /tmp/pkcs11.cfg
-chmod 755 /tmp/pkcs11.cfg
-echo "name = ${key_label}" >> /tmp/pkcs11.cfg
-echo "${applicationlibrary}"
-echo "library = ${applicationlibrary}" >> /tmp/pkcs11.cfg
-echo "slot = ${SoftHSMv2SlotID}" >> /tmp/pkcs11.cfg
+else
 
-# 5. Compile the Application
-cd /tmp/files/applicationfiles
-cp test.csr /tmp/test.csr
-javac CaSign.java
+    # 2.a SoftHSM mode implementation
 
-# 6. Convert the crt to der format
-openssl x509 -in ca.cert -outform der -out ca.der
+    echo "There is no Plugin directory assigned. Hence Falling
+          back to SoftHSM implementation"
 
-# 7. Add the ca certificate
-pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so -l --pin ${upin} --write-object ./ca.der --type cert --id ${cert_id}
+    cd /tmp/files
 
-# 8. Run the Application
-java CaSign ${upin} 0x${cert_id}
+    # 2.b Convert the Private key pem into der format
+    openssl rsa -in ./privkey.pem -outform DER -out privatekey.der
 
-# 9. Verify the generated certificate
-openssl verify -verbose -CAfile ca.cert /tmp/test.cert
+    softhsm2-util --init-token --slot ${slot_no} --label "${token_name}" --pin ${upin} --so-pin ${sopin}
+    softhsm2-util --show-slots | grep 'Slot ' | cut -d\           -f2 | head -1 >> slotinfo.txt
+    export SoftHSMv2SlotID="$(cat slotinfo.txt)"
+    echo "${SoftHSMv2SlotID}"
 
-# 10. CleanUp
+    # 2.c Load the Private key into SoftHSM
+    pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so -l --pin ${upin}
+--write-object ./privatekey.der --type privkey --id ${cert_id} --label ${key_label}
+
+fi
+
+# 3. Calling the functionalities of the sample application
 cd /
-rm -rf slotinfo.txt
+chmod 755 application.sh
+./application.sh
