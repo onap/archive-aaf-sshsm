b4133b73e86c4f8cbd4284dde9970c2e8216baa4
Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>
2018-08-23 15:30:53 -0700

    Add build script for initabrmd container
    Add script to build and push the image to
    nexus repo. Invoke the script from top level
    build script.
    
    Change-Id: I644428d2da3bbc4688a3a45d34b8b7e9148314b0
    Issue-ID: AAF-443
    Signed-off-by: Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>

--- bin/build_images.sh
+++ bin/build_images.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
+
 cd base
+chmod 755 build_base_images.sh
 ./build_base_images.sh
+
 cd ../distcenter
+chmod 755 build_distcenter_image.sh
 ./build_distcenter_image.sh
+
+cd ../abrmdcontainer
+chmod 755 build_abrmd_image.sh
+./build_abrmd_image.sh
