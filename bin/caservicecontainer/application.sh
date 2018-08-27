fdf5561fa8d7c583bddb619c38e8890835f2952b
Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>
2018-08-27 09:05:45 -0700

    Script for SoftHSMv2 fallback mechanism
    This will facilitate the SoftHSMv2 implementation
    when TPM is unavailable
    
    Change-Id: Ic77627702db514213cece200a259f723e6d66d34
    Issue-ID: AAF-414
    Signed-off-by: Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>

---
+++ bin/caservicecontainer/application.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Location to fecth SoftHSM library required by application
+applicationlibrary="/usr/local/lib/softhsm/libsofthsm2.so"
+
+# Setting up the java application and running the application
+# 1. Create the configuration pkcs11.cfg for the application
+touch /tmp/pkcs11.cfg
+chmod 755 /tmp/pkcs11.cfg
+echo "name = ${key_label}" >> /tmp/pkcs11.cfg
+echo "The location of applicationms library is ${applicationlibrary}"
+echo "library = ${applicationlibrary}" >> /tmp/pkcs11.cfg
+echo "slot = ${SoftHSMv2SlotID}" >> /tmp/pkcs11.cfg
+
+# 2. Compile the Application
+cd /tmp/files
+cp test.csr /tmp/test.csr
+javac CaSign.java
+
+# 3. Run the Application
+java CaSign ${upin} 0x${cert_id}
+
+# 4. Verify the generated certificate
+openssl verify -verbose -CAfile ca.cert /tmp/test.cert
+
+# 5. Cleanup
+cd /
+rm -rf slotinfo.txt
