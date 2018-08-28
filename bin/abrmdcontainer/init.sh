721eb7117974203e93631db5cf0b06f1579a512a
Kiran Kamineni <kiran.k.kamineni@intel.com>
2018-08-28 16:24:42 -0700

    Modify initialization to remove ownership step
    TPM ownership is assumed to be a step that is
    executed by the system administrator who will
    provide the credentials to pods during startup
    
    Issue-ID: AAF-409
    Change-Id: Id0d1860f257c98045613e90b6b88e37858a9aceb
    Signed-off-by: Kiran Kamineni <kiran.k.kamineni@intel.com>

--- bin/abrmdcontainer/init.sh
+++ bin/abrmdcontainer/init.sh
@@ -2,42 +2,14 @@
 
 set -e
 
-echo "Shared volume is ${ABRMD_DATA}"
-FILE="${ABRMD_DATA}/tpm_status.yaml"
-if [ -f $FILE ];then
-    flag=$(echo "$(cat ${ABRMD_DATA}/tpm_status.yaml)" | sed '/^flag/{s/[^0-9,]//g;y/,/\n/;}')
-    if [ "$flag" == 0 ];then
-        # Start DBUS
-        mkdir -p /var/run/dbus
-        stdbuf -oL -eL dbus-daemon --system --nofork 2>&1 1> /var/log/dbus-daemon.log &
-        # Time for Daemon to start before executin next step
-        sleep 1m
-        # Start Resource Manager
-        if [ -z $TPM_SIMULATOR ]; then
-            echo "Using TPM Hardware for the operations"
-            tpm2-abrmd &
-            # Time for abrmd process to start
-            sleep 1m
-            state=$( ps aux | grep tpm2-abrmd | grep -v grep )
-            echo "Staus of abrmd Process is $state"
-        else
-            echo "Using TPM Simulator for the opeations";
-            hostip=$(ip route show | awk '/default/ {print $3}');
-            echo "Connecting to $hostip\n";
-            tpm2-abrmd -a $hostip -t socket&
-        fi
+echo "Shared Volume location is $ABRMD_DATA"
+echo "Node name is $TPM_NODE_NAME"
 
-        /abrmd/bin/initialize_tpm.sh
-        status=$?
-        if [ $status -eq "0" ]; then
-            echo "TPM Initialization successful $status"
-        fi
-        exit $?
-    else
-        echo "TPM is already Initialized"
-        exit;
-    fi
-else
-    echo " TPM Status file not found, Hence exiting"
-    exit;
+/abrmd/bin/run_abrmd.sh &
+sleep 2m
+/abrmd/bin/initialize_tpm.sh
+status=$?
+if [ $status -eq "0" ]; then
+    echo "TPM Initialization successful $status"
 fi
+exit $status
