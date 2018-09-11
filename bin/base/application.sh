f55631def9fb3a7c3e5f81ce2af14311940a756a
Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>
2018-09-11 09:11:06 -0700

    Add import scripts to base image
    These scripts imports the CA key to either tpm or
    softhsm. Updates the pkcs11 config file and adds
    the required config for softhsm
    
    Change-Id: If45cfb514756bf4ab03081d458ed728921fa1d51
    Issue-ID: AAF-483
    Signed-off-by: Manjunath Ranganathaiah <manjunath.ranganathaiah@intel.com>

---
+++ bin/base/application.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# This script receives 2 arguments
+applicationlibrary=$1
+SoftHSMv2SlotID=$2
+
+# Setting up the pkcs11 config file
+echo "library = ${applicationlibrary}" >> /opt/app/osaaf/local/org.osaaf.aaf.cm.pkcs11
+echo "slot = ${SoftHSMv2SlotID}" >> /opt/app/osaaf/local/org.osaaf.aaf.cm.pkcs11 
