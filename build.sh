9c7cd15316f703ed2b625b261d236f0f2770199f
Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>
2019-03-21 09:14:45 -0700

    fix check errors in the build file
    Issue-ID: AAF-793
    Change-Id: I79f17ad508a29ff0239b7504cd8ab2ce5e11e8bb
    Signed-off-by: Pramod Raghavendra Jayathirth <pramod.raghavendra.jayathirth@intel.com>

--- build.sh
+++ build.sh
@@ -1,62 +1,45 @@
 #!/bin/bash
 
-#set -e
-sudo kill -9 $(ps -ef | grep "apt" | grep -v grep | awk '{print $2}')
-
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/lib/apt/lists/lock
-sudo rm /var/cache/apt/archives/lock
-
-sudo dpkg --configure -a
-
-sudo dpkg --remove --force-remove-reinstreq linux-headers-generic
-sudo dpkg --remove --force-remove-reinstreq linux-generic
-sudo apt-get -f install
-sudo apt-get install linux-image-generic
+set -e
 
 sudo apt -y update
-sudo apt -y install autoconf
-sudo apt -y install autoconf-archive
-sudo apt -y install libglib2.0-dev
-sudo apt -y install libdbus-1-dev
-sudo apt -y install automake
-sudo apt -y install libtool
-sudo apt -y install autotools-dev
-sudo apt -y install libcppunit-dev
-sudo apt -y install p11-kit
-sudo apt -y install libcurl4-gnutls-dev
-sudo apt -y autoremove
+
+sudo apt-get -y install \
+    autoconf \
+    autoconf-archive \
+    libglib2.0-dev \
+    libdbus-1-dev \
+    automake \
+    libtool \
+    autotools-dev \
+    libcppunit-dev \
+    p11-kit \
+    libcurl4-gnutls-dev \
+    libcmocka0 \
+    libcmocka-dev \
+    build-essential \
+    git \
+    pkg-config \
+    vim \
+    gcc \
+    g++ \
+    m4 \
+    curl \
+    wget \
+    liburiparser-dev \
+    libssl-dev \
+    pandoc \
+    opensc \
+    default-jdk
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/
 
 echo "Build SoftHSMv2..."
 cd SoftHSMv2
 sh autogen.sh
 ./configure --disable-gost
-make check
-make -j$(nproc)
+sudo make install
 cd ..
-
-sudo apt -y install \
-  libcmocka0 \
-  libcmocka-dev \
-  build-essential \
-  git \
-  pkg-config \
-  gcc \
-  g++ \
-  m4 \
-  liburiparser-dev \
-  libgcrypt20-dev \
-  libssl-dev \
-  pandoc
-
-#echo "Install openssl..."
-#git clone https://github.com/openssl/openssl.git
-#cd openssl
-#./config
-#make -j$(nproc)
-#sudo make install
-#cd ..
-#rm -rf openssl
 
 echo "Install tpm2-tss 1.2.0..."
 git clone https://github.com/tpm2-software/tpm2-tss.git
@@ -107,4 +90,3 @@ make
 sudo make install
 sudo ldconfig
 cd ..
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
